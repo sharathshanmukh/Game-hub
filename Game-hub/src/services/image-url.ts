7cb818198d36a633740006ad320d9f718d65472e
sharatsharathshanmukh <sarathponugupati@gmail.com>
2023-12-22 03:44:10 -0500

    handlkegamez no img

--- Game-hub/src/services/image-url.ts
+++ Game-hub/src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp"
 export const getCroppedImageUrl=(url:string)=>{
-    if (!url) return "";
+    if (!url) return noImage;
     const word="media/"
     const index=url.indexOf(word)+word.length
     return url.slice(0,index)+"crop/600/400/"+url.slice(index)
